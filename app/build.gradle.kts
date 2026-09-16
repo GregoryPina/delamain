7128b7c97229a9ea1fa1d9b9cad49e70e453fa88
GregoryPina <112197829+GregoryPina@users.noreply.github.com>
2026-09-16 12:24:28 -0300

    TASK-014 add Preferences DataStore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,13 +31,8 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
-    buildFeatures {
-        compose = true
-    }
+    kotlinOptions { jvmTarget = "17" }
+    buildFeatures { compose = true }
 }
 
 dependencies {
@@ -51,6 +46,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.datastore:datastore-preferences:1.1.2")
     testImplementation("junit:junit:4.13.2")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
